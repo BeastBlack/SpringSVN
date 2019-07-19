82d82b4ff7523fc457a11ddc4dd336b32a61b76c
norbert <norbert.zielinski@bzwbk.pl>
2019-07-19 12:29:08 +0200

    Aktualizacja pom, oczyszczenie testów

--- springsvn/src/test/java/dev/blackbeast/springsvn/SpringsvnApplicationTests.java
+++ springsvn/src/test/java/dev/blackbeast/springsvn/SpringsvnApplicationTests.java
@@ -1,29 +1,17 @@
 package dev.blackbeast.springsvn;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//@SpringBootTest
+@SpringBootTest
 class SpringsvnApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Pattern pattern = Pattern.compile("(TEET-)\\d{1,}");
-		Matcher m = pattern.matcher("TEET-10000 abc");
 
-		StringBuffer bufStr = new StringBuffer();
-
-		boolean flag = false;
-		while ((flag = m.find())) {
-			String rep = m.group();
-			m.appendReplacement(bufStr, "<a th:href=\"abc\">" + rep + "</a>");
-		}
-
-		m.appendTail(bufStr);
-		String result = bufStr.toString();
-		System.out.println(result);
 	}
 
 }
