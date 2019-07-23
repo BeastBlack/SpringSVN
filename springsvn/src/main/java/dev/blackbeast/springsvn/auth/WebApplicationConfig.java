ff93451e6938d4441717ab11cf8c63513ed37a92
norbert <norbert.zielinski@bzwbk.pl>
2019-07-23 11:02:30 +0200

    Dodanie template-resolver'a

--- springsvn/src/main/java/dev/blackbeast/springsvn/auth/WebApplicationConfig.java
+++ springsvn/src/main/java/dev/blackbeast/springsvn/auth/WebApplicationConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class WebApplicationConfig implements WebMvcConfigurer {
@@ -21,5 +23,18 @@ public class WebApplicationConfig implements WebMvcConfigurer {
             container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN,
                     "/access-denied"));
         };
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver yourTemplateResolver() {
+        ClassLoaderTemplateResolver configurer = new ClassLoaderTemplateResolver();
+        configurer.setPrefix("templates/");
+        configurer.setSuffix(".html");
+        configurer.setTemplateMode(TemplateMode.HTML);
+        configurer.setCharacterEncoding("UTF-8");
+        configurer.setOrder(0);  // this is important. This way spring //boot will listen to both places 0 and 1
+        configurer.setCacheable(false);
+        configurer.setCheckExistence(true);
+        return configurer;
     }
 }
